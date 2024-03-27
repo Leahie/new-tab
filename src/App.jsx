64a8e020d54abd0b9c600e566ca11b2230a8ccc1
Leah Zhang <LeahZhang1595995@gmail.com>
2024-03-27 12:22:04 -0400

    f

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/* import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg' */
+import Header from "./components/header.jsx"
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      < Header/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,8 @@
+export default function  Header(){
+    return(
+        <div className="Header">
+            <h1>"message"</h1>
+            <h3>"time"</h3>
+        </div>
+    )
+}
