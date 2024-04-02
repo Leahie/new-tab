29581491ee5b169203bcfb1bae4c522fb05f602f
Leah Zhang <LeahZhang1595995@gmail.com>
2024-04-02 15:11:46 -0400

    fixed 12 I thnk

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -24,7 +24,7 @@ export default function  Header({person = "Leah"}){
     const text = time.split(":");
     const mood = text[2].split(" ");
     let t = parseInt(text[0]);
-    mood[1] == "PM" ? t = 12+t : t;
+    mood[1] == "PM" && t!=12 ? t = 12+t : t;
     mood[1] == "AM" && t==12 ? t=0 : t=t;
 
     return(
