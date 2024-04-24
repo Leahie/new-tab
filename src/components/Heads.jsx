c53dd26c09ac14457ad84d1e4dd28c711dc42778
Leah Zhang <LeahZhang1595995@gmail.com>
2024-04-24 08:56:34 -0400

    fixed time

--- src/components/Heads.jsx
+++ src/components/Heads.jsx
@@ -4,7 +4,7 @@ export default function Heading({t}){
     console.log(t)
     return(
         <div className="Heading">
-            {t>6 && t<=9 && <h1>Why are you awake</h1>}
+            {t>=6 && t<8 && <h1>Why are you awake</h1>}
             {t>=8 && t<12 && <h1>Good morning, Leah!</h1> }
             {t>=12 && t<14 && <h1>Good afternoon, Leah!</h1>}
             {t>=14 && t<20 && <h1>Good evening, Leah!</h1>}
